44322809c9e4742dfdfa3f3a26a14a84bc9dab6b
manthan <manthanmji@gmail.com>
2024-10-20 22:56:15 +0530

    added bottom nav

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 #include <vector>
 
 const int screenWidth = 800;
-const int screenHeight = 600;
+const int screenHeight = 650;
 const int gridSize = 80;
-const int gridWidth = screenWidth / gridSize;
-const int gridHeight = screenHeight / gridSize;
+const int gridWidth = 800 / gridSize;
+const int gridHeight = 600 / gridSize;
 const float highlightThreshold = 5.0f; 
 
 using namespace std;
@@ -26,6 +26,8 @@ typedef struct {
     int value;
     Color c;
     float I;
+    char* name;
+    int selected;
 } Node;
 
 
@@ -133,6 +135,7 @@ Node* get(umap &m, Node ne) {
         }
     }
 }
+
 Node* set(umap &m, Node ne, float i) {
     for(auto &[key, value]: m) {
         for(auto &n: value) {
@@ -143,6 +146,20 @@ Node* set(umap &m, Node ne, float i) {
             }
             if(t.x == ne.from.x && t.y == ne.from.y && f.x == ne.to.x && f.y == ne.to.y) {
                 n.I += i;
+            }
+        }
+    }
+}
+void setv(umap &m, Node ne, float v) {
+    for(auto &[key, value]: m) {
+        for(auto &n: value) {
+            Vector2 f = n.from;
+            Vector2 t = n.to;
+            if(f.x == ne.from.x && f.y == ne.from.y && t.x == ne.to.x && t.y == ne.to.y) {
+                n.value = v;
+            }
+            if(t.x == ne.from.x && t.y == ne.from.y && f.x == ne.to.x && f.y == ne.to.y) {
+                n.value = v;
             }
         }
     }
@@ -160,10 +177,14 @@ void solve(vector<vector<Node>> *loops, umap &m) {
             ne->I=0;
         }
     }
+
     
     for(auto loop: *loops) {
-        int numres = loop.size() -1;
-        float i = 1.0/numres;
+        float numres = 0.0001;
+        for(auto n: loop) {
+            numres += n.value;
+        }
+        float i = loop[0].value/numres;
         for(auto n: loop) {
             // Node* ne = get(m, n);
             // ne->I+=i;
@@ -193,6 +214,7 @@ void DrawResistor(Vector2 from, Vector2 start, Color c, int thickness) {
     start.x = start.x- from.x;
     start.y = start.y- from.y;
     float foc = 0.17;
+    
     DrawLineEx((Vector2) {from.x, from.y}, (Vector2) {start.x*foc+from.x, start.y*foc+from.y}, thickness, c);
     if(from.y == starto.y) {
         DrawLineEx((Vector2){start.x*foc+from.x, start.y*foc+from.y}, (Vector2){start.x*foc+from.x + 5, start.y*foc+from.y+5}, thickness, c);
@@ -219,10 +241,23 @@ void DrawResistor(Vector2 from, Vector2 start, Color c, int thickness) {
 void DrawDevice(Node device, Color c, int t) {
     c = device.c;
     if(device.type == RESISTOR) {
+        if(device.value == 0) {
+            DrawLineEx(device.from, device.to, t, device.c);
+        } else 
         DrawResistor(device.from, device.to, c, t);
     } else {
         DrawBattery(device.from, device.to, c);
     }
+}
+
+bool IsAnyKeyPressed()
+{
+    bool keyPressed = false;
+    int key = GetKeyPressed();
+
+    if ((key >= 32) && (key <= 126)) keyPressed = true;
+
+    return keyPressed;
 }
 
 int main() {
@@ -233,6 +268,15 @@ int main() {
     SetTargetFPS(60);
     int num=0;
     int written=1;
+    int hne= 0;
+    Node highlighted;
+
+    char name[40 + 1] = "\0";      // NOTE: One extra space required for null terminator char '\0'
+    int letterCount = 0;
+
+    Rectangle textBox = { 140, 615, 60, 30 };
+    bool mouseOnText = false;
+    
     while (!WindowShouldClose()) {
         ClearBackground(BLACK);
 
@@ -269,7 +313,8 @@ int main() {
             }
         }
 
-        BeginDrawing();
+        DrawRectangle(0, 560, screenWidth, screenHeight, WHITE);        
+
         vector<vector<Node>> loops;
         Node batry;
         for (const auto& [key, devicess] : devices) {
@@ -288,17 +333,111 @@ int main() {
         cout << "START "<< loops.size() << endl;
         if(loops.size() > num)
             for(int j=0; j<loops[num].size(); j++) {
-                loops[num][j].c = BLUE;
+                loops[num][j].c = ORANGE;
                 DrawDevice(loops[num][j], BLUE, 2);
                 Node* n = get(devices, loops[num][j]);
                 if(!written) {cout << loops[num][j].from.x<< ", " << loops[num][j].from.y<< " to " << loops[num][j].to.x << ", "<< loops[num][j].to.y << " "<< n->I<<endl;
-                }
+            }
         }
         if(loops.size() > num) written = true;
         if(!written)
         cout << "END" << endl;
-        EndDrawing();
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            if(mousePos.y < 600) hne=0;
+            if(t) {
+                if(devices.find(activeLine.first) != devices.end()) {
+                    for(auto dv : devices[activeLine.first]) {
+                        for(auto dt: devices[activeLine.second]) {
+                            if(dv.from.x == dt.from.x && dv.to.x == dt.to.x  && dv.from.y == dt.from.y && dv.to.y == dt.to.y) {
+                                highlighted = dv;
+                                hne = 1;
+                                sprintf(name, "%d", get(devices, highlighted)->value);
+                                letterCount = 0;
+                                break;
+                            }
+                            if(dv.to.x == dt.from.x && dv.from.x == dt.to.x  && dv.to.y == dt.from.y && dv.from.y == dt.to.y) {
+                                highlighted = dv;
+                                hne = 1;
+                                sprintf(name, "%d", get(devices, highlighted)->value);
+                                letterCount = 0;
+                                break;
+                            }
+                        }
+                        if(dv.type == BATTERY) {
+                            highlighted=dv;
+                            sprintf(name, "%d", get(devices, highlighted)->value);
+                            letterCount = 0;
+                            hne = 1;
+                        }
+                    }
+                }
+            }
+        }
 
+        /////////////////////////////////////// MOUSE HANDLING FOR INPUT FIELD
+
+
+
+        if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOnText = true;
+        else mouseOnText = false;
+
+        if (mouseOnText)
+        {
+            // Set the window's cursor to the I-Beam
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+
+            // Get char pressed (unicode character) on the queue
+            int key = GetCharPressed();
+
+            // Check if more characters have been pressed on the same frame
+            while (key > 0)
+            {
+                // NOTE: Only allow keys in range [32..125]
+                if ((key >= 32) && (key <= 125) && (letterCount < 10))
+                {
+                    name[letterCount] = (char)key;
+                    name[letterCount+1] = '\0'; // Add null terminator at the end of the string.
+                    letterCount++;
+                }
+
+                key = GetCharPressed();  // Check next character in the queue
+            }
+
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                letterCount--;
+                if (letterCount < 0) letterCount = 0;
+                name[letterCount] = '\0';
+            }
+            if(hne ) {
+                cout << stoi(name) << "HERE" << endl;;
+                setv(devices, highlighted, stoi(name));
+            }
+        }
+        
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+        DrawRectangleRec(textBox, LIGHTGRAY);
+        if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
+        else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
+
+        DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 20, MAROON);
+
+        // DrawText(TextFormat("INPUT CHARS: %i/%i", letterCount, 10), 315, 250, 20, DARKGRAY);
+
+
+        ///////////////////////////////////
+        DrawRectangle(20, 570, 70, 70, DARKGRAY);
+        if(hne) {
+            // cout << "HIGHLGHTE" << endl;
+            highlighted.from = { 20, 610};
+            highlighted.to  =  { 100, 610};
+            highlighted.c = WHITE;
+            DrawDevice(highlighted, PINK, 3);
+            DrawText(TextFormat("Type: %s", (highlighted.type)?"Battery":"Resistor"), 120, 570, 20, BLACK);
+            DrawText(TextFormat("Value: %d%s", highlighted.value, (highlighted.type)?"V":"ohm"), 120, 590, 20, BLACK);
+        }
+        
+        EndDrawing();
 
         if (IsKeyPressed(KEY_ZERO)){ num = 0; written = 0; }
         if (IsKeyPressed(KEY_ONE)){ num = 1; written = 0; }
@@ -319,10 +458,18 @@ int main() {
                 devices[activeLine.second].push_back(n);
             }
         }
+        if (IsKeyPressed(KEY_W) && IsKeyDown(KEY_LEFT_SHIFT)) {
+            if(t) {
+                Node n = getEmptyNode(activeLine.first, activeLine.second, RESISTOR);
+                n.value = 0;
+                devices[activeLine.first].push_back(n);
+                devices[activeLine.second].push_back(n);
+            }
+        }
         if (IsKeyPressed(KEY_B) && IsKeyDown(KEY_LEFT_SHIFT)) {
             if(t) {
                 Node n = getEmptyNode(activeLine.first, activeLine.second, BATTERY);
-                n.value = 1;
+                n.value = 10;
                 devices[activeLine.first].push_back(n);
             }
         }
